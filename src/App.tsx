75dd621daa137e1a562c3d059a0b5c5c611014a9
Taylan <taylancankose@gmail.com>
2024-02-23 21:17:34 +0300

    bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ import NoResult from "./components/NoResult.tsx";
 
 function App() {
   const [pokemons, setPokemons] = useState<PokeType>();
-  const [pokeDetail, setPokeDetail] = useState<undefined | PokeDetailsType>();
+  const [pokeDetail, setPokeDetail] = useState<{} | PokeDetailsType>();
   const [showDetails, setShowDetails] = useState(false);
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   const [loading, setLoading] = useState(false);
