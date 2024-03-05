e42d2a1a2386facbfe3de6917d34d28e322e3a21
Taylan <taylancankose@gmail.com>
2024-03-05 16:29:45 +0300

    pagination bug fix.

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -16,7 +16,7 @@ function Pagination({ pokesPerPage, totalPokes, paginate }: PageTypes) {
       <ul className="pagination">
         {pageNumbers.map((number) => (
           <li key={number} className="page-item">
-            <a onClick={() => paginate(number)} href="!#" className="page-link">
+            <a onClick={() => paginate(number)} className="page-link">
               {number}
             </a>
           </li>
